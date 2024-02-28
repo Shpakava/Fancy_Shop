e6ef8803040a689ea5f269e20b8750878ab68fe2
python <python@test.com>
2024-02-28 21:11:33 +0300

    create models Category, Product in project

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Product)
--- config/settings/common.py
+++ config/settings/common.py
@@ -123,9 +123,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#all pictures
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+# отслеживает id и увеличивает его на единицу автоматически
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
