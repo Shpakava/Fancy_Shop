7855113ebecffae7bb2cfd19f37aacca85b3980f
python <python@test.com>
2024-04-03 21:08:15 +0300

    create edit profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from profiles.views import *
 
 urlpatterns = [
     path("<slug:profile_slug>", get_profile_details, name='profile_details'),
+    path("<slug:profile_slug>/edit", edit_profile_details, name='profile_edit')
 ]
